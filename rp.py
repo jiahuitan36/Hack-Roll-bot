702fd656ec1f1cb8952e8bd7da6a9432f95d81a6
Tan Jia Hui <60168038+jiahuitan36@users.noreply.github.com>
2022-01-08 01:08:27 +0800

    Add files via upload

---
+++ rp.py
@@ -0,0 +1,20 @@
+dictionary = {
+    85 : ["Law", "Medicine", "Dentistry", "Business Analytics", "Computer Science", "Information Systems", "Information Security", "Pharmaceutical Science", "Science (Food Science & Technology)", "Arts & Social Sciences (Philosophy, Politics & Economics)"],
+    83.75 : ["Computer Engineering", "Pharmacy"],
+    82.5 : ["Data Science and Analytics", "Science", "Environmental Studies"],
+    81.25 : ["Business Administration (Accountancy)"],
+    80 : ["Business Administration"],
+    78.75 : ["Mechanical Engineering (Aeronautical)"],
+    76.25 : ["Chemical Engineering", "Arts & Social Sciences"],
+    75 : ["Biomedical Engineering"],
+    73.75 : ["Industrial Design", "Electrical Engineering", "Engineering Science", "Mechanical Engineering", "Arts & Social Sciences (MT related)"],
+    72.5 : ["Environmental Engineering", "Industrial & Systems Engineering", "Materials Science & Engineering"],
+    70 : ["Architecture", "Landscape Architecture"],
+    68.75 : ["Project & Facilities Management"],
+    67.5 : ["Civil Engineering"],
+    66.25 : ["Nursing", "Real Estate"]
+}
+
+# print(dictionary.keys())
+# for key, value in dictionary.items():
+#     print(key, value)
